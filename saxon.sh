3bfc313186919457fef922673998c75ee3751776
Robbie Hott <robbie@robbiehott.com>
2013-09-06 13:09:27 -0400

    Updated java extension handler. Added date parser that understands most standard date formats and exports to xsl a YYYY-MM-DD formatted date. Now requires Apache Commons Libraries.

--- saxon.sh
+++ saxon.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export CLASSPATH=$HOME/bin/saxon9he.jar:$CLASSPATH
-java net.sf.saxon.Transform ${1+"$@"}
+export CLASSPATH=$HOME/saxon/saxon9he.jar:$HOME/javarepo/commons-lang3-3.1/commons-lang3-3.1.jar:$HOME/snac_utils/java/bin:$CLASSPATH
+java edu.virginia.iath.snac.SnacTransform ${1+"$@"}
