91d35590ae096f0aaf9f5a9db3eed221ca7b202f
Robbie Hott <robbie@robbiehott.com>
2013-09-06 15:48:09 -0400

    Added handling for circa

--- src/edu/virginia/iath/snac/helpers/DateParserHelper.java
+++ src/edu/virginia/iath/snac/helpers/DateParserHelper.java
@@ -1,6 +1,7 @@
 package edu.virginia.iath.snac.helpers;
 
 import java.text.ParseException;
+import java.util.Calendar;
 import java.util.Date;
 
 import org.apache.commons.lang3.time.DateFormatUtils;
@@ -9,18 +10,19 @@ import org.apache.commons.lang3.time.DateUtils;
 public class DateParserHelper {
 	
 	private String dateString = null;
-	private String date1Str = null;
-	private String date2Str = null;
-	private Date date1 = null;
-	private Date date2 = null;
-	private String date1StrModifier = null;
-	private String date2StrModifier = null;
+	private String[] dateStr = null;
+	private Date[] dates = null;
+	private String[] dateStrModifier = null;
 	private static String outputFormat = "yyyy-MM-dd";
 	
 
-	public DateParserHelper(String dateStr) {
+	public DateParserHelper(String d) {
+		dates = new Date[2];
+		dateStr = new String[2];
+		dateStrModifier = new String[2];
+		
 		// Store the date string locally
-		dateString = dateStr.trim();
+		dateString = d.trim();
 		
 		// Parse the dates into Date objects
 		runParser();
@@ -29,45 +31,74 @@ public class DateParserHelper {
 	private void runParser() {
 		// Check for date range.  If so, parse separately
 		if (dateString.contains("-")) {
-			date1Str = dateString.substring(0, dateString.indexOf("-")).trim();
-			date2Str = dateString.substring(dateString.indexOf("-") + 1).trim();
-
-			date1 = parseDate(date1Str);
-			date2 = parseDate(date2Str);
+			dateStr[0] = dateString.substring(0, dateString.indexOf("-")).trim();
+			dateStr[1] = dateString.substring(dateString.indexOf("-") + 1).trim();
+			
+			// parse the two dates
+			parseDate(0);
+			parseDate(1);
 		} else {
-			date1Str = dateString.trim();
-			date1 = parseDate(dateString);
+			dateStr[0] = dateString.trim();
+			dates[1] = null;
+			dateStr[1] = null;
+			
+			// parse the date
+			parseDate(0);
 		}
 	}
 
 	public boolean isRange() {
-		return date2 != null;
+		return dates[1] != null;
 	}
 
 	public String firstDate() {
-		return getOutputString(date1);
+		return getOutputString(dates[0]) + " " + handleModifier(0);
 	}
 
 	public String secondDate() {
-		return getOutputString(date2);
+		return getOutputString(dates[1]) + " " + handleModifier(0);
 	}
 
 	public String getDate() {
-		return getOutputString(date1);
+		return getOutputString(dates[0]) + " " + handleModifier(0);
 	}
 	
-	private Date parseDate(String d) {
+	private void parseDate(int i) {
+		dateStrModifier[i] = null;
+		
+		if (dateStr[i].contains("circa")) {
+			dateStrModifier[i] = "circa";
+			dateStr[i] = dateStr[i].replace("circa", "");
+		}
+		
 		try {
 			// Currently we are ignoring "-" in the text, since that is used for ranges in dates
-			return DateUtils.parseDate(d.trim(),
+			dates[i] = DateUtils.parseDate(dateStr[i].trim(),
 					"yyyy", /*"yyyy-MM", "yyyy-M", "yyyy-M-d", "yyyy-M-dd", "yyyy-MM-d", "yyyy-MM-dd",*/ // standard dates
 					"MMMMM dd, yyyy", "MMM dd, yyyy", "MMM dd yyyy", "MMMMM dd, yyyy", "yyyy MMM dd",
 					"dd MMM, yyyy", "dd MMMMM, yyyy", "yyyy, MMM dd", "yyyy, MMMMM dd", "yyyy, MMM. dd",
 					"MMMMM yyyy", "MMM yyyy"
 					);
 		} catch (ParseException e) {
-			return null;
+			dates[i] = null;
 		}
+	}
+	
+	private String handleModifier(int i) {
+		String output = "";
+		if (dateStrModifier[i] != null) {
+			if (dateStrModifier[i].equals("circa")) {
+				Calendar d = Calendar.getInstance();
+				d.setTime(dates[i]);
+				d.add(Calendar.YEAR, -3);
+				output += "( " + DateFormatUtils.format(d.getTime(), outputFormat) + " -- ";
+				d.setTime(dates[i]);
+				d.add(Calendar.YEAR, 3);
+				output += DateFormatUtils.format(d.getTime(), outputFormat) + ")";
+				
+			}
+		}
+		return output;
 	}
 	
 	private String getOutputString(Date d) {
