c3e6f63ec8f334808a3d15f0e7efccdb2772d73e
Robbie Hott <robbie@robbiehott.com>
2013-10-09 12:36:53 -0400

    Updated shell script

--- saxon.sh
+++ saxon.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-export CLASSPATH=$HOME/saxon/saxon9he.jar:$HOME/jars/*.jar:$CLASSPATH
+#
+# Setup of the script
+#
+# Set this to the path where you downloaded the saxon JAR file
+export SAXON_HOME=/media/shannon/saxon/saxon9he.jar
+
+# Set this to the path where you downloaded the Apache Commons JAR
+export APACHE_COMMONS=/media/shannon/javarepo/commons-lang3-3.1/commons-lang3-3.1.jar
+
+# Set this to the path where you compiled the Java source
+export SNAC_JAVA=/media/shannon/SNAC-Saxon-Extensions/java/bin
+
+export CLASSPATH=$SAXON_HOME:$APACHE_COMMONS:$SNAC_JAVA:$CLASSPATH
 java edu.virginia.iath.snac.SnacTransform ${1+"$@"}
