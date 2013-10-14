97478bba55eb34828cf319d2e794b579f2d9f948
deternitydx <roby2ka@hotmail.com>
2013-10-14 13:21:44 -0400

    Update saxon.sh

--- saxon.sh
+++ saxon.sh
@@ -9,7 +9,7 @@ export SAXON_HOME=/media/shannon/saxon/saxon9he.jar
 export APACHE_COMMONS=/media/shannon/javarepo/commons-lang3-3.1/commons-lang3-3.1.jar
 
 # Set this to the path where you compiled the Java source
-export SNAC_JAVA=/media/shannon/SNAC-Saxon-Extensions/java/bin
+export SNAC_JAVA=java/bin
 
 export CLASSPATH=$SAXON_HOME:$APACHE_COMMONS:$SNAC_JAVA:$CLASSPATH
 java edu.virginia.iath.snac.SnacTransform ${1+"$@"}
